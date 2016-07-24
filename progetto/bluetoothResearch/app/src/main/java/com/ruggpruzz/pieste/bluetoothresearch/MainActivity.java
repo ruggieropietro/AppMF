0da34ab073c9d61645b10060d0d3099e7d9e8c13
ruggieropietro <ruggiero.pietro@hotmail.it>
2016-07-24 15:13:33 +0200

    ricerca dipositivi ma annuncio su toast

--- progetto/bluetoothResearch/app/src/main/java/com/ruggpruzz/pieste/bluetoothresearch/MainActivity.java
+++ progetto/bluetoothResearch/app/src/main/java/com/ruggpruzz/pieste/bluetoothresearch/MainActivity.java
@@ -16,8 +16,10 @@
 //da un toast.
 package com.ruggpruzz.pieste.bluetoothresearch;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -27,13 +29,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Set;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    private BluetoothAdapter mBluetoothAdapter ;
+    private BluetoothAdapter btAdapter;
+    private Set<BluetoothDevice> dispositivi ;
+    private ListView lv  ;
+    private ArrayAdapter<String> adapter =null;
+    private static final int BLUETOOTH_ON=1000;
+
+    private final BroadcastReceiver mReceiver = new BroadcastReceiver() {
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            // When discovery finds a device
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                // Get the BluetoothDevice object from the Intent
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                // Add the name and address to an array adapter to show in a ListView               // String blue =device.getName();
+                //adapter.add(blue);
+                Toast.makeText(MainActivity.this, device.getName(), Toast.LENGTH_LONG).show();
+
+            }
+        }
+    };
 //qui viene creato il broadcast receiver e inpostato per ascoltare il bluetooth e i suoi stati generalizzati
     private final BroadcastReceiver mBroadcastReceiver1 = new BroadcastReceiver() {
 
@@ -64,28 +90,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+        String[] bluetoothname ={""};
+        adapter=new  ArrayAdapter(this,android.R.layout.simple_list_item_1,bluetoothname);
+        ListView lv = (ListView)findViewById(R.id.listView);
+        lv.setAdapter(adapter);
+
+
 
         //qui nel onCreate la memorizzazione formale di stati precedenti per il controllo di cambio
         //degli stati
         IntentFilter filter1 = new IntentFilter(BluetoothAdapter.ACTION_STATE_CHANGED);
         registerReceiver(mBroadcastReceiver1, filter1);
-        setContentView(R.layout.activity_main);
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        IntentFilter filter2 = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter2);
+
+
         TextView tv = (TextView) findViewById(R.id.textView);
         TextView req = (TextView) findViewById(R.id.textView2);
         //qui il controllo con textView e toast riguardo la verifica di esistenza del bluetooth interno
         if (mBluetoothAdapter == null) {
-            tv.setText("Bluetooth not found!");
+            tv.setText("Internal Bluetooth not found!");
             Toast.makeText(MainActivity.this, "Internal bluetooth not found! ", Toast.LENGTH_LONG).show();
         } else {
-            tv.setText("Bluetooth  found!");
+            tv.setText("InternalBluetooth  found!");
             Toast.makeText(MainActivity.this, "Internal bluetooth found! ", Toast.LENGTH_LONG).show();
             if (!mBluetoothAdapter.isEnabled()) {
-                req.setText("Bluetooth disabled");
                 mBluetoothAdapter.enable();
-                req.setText("Bluetooth abled");
+                req.setText("Bluetooth abled!");
             } else {
-                req.setText("Bluetooth abled");
+                mBluetoothAdapter.disable();
+                req.setText("Bluetooth disabled!");
             }
         }
         //qui l'ascolto al tasto per abilitare o disabilitare il bluetooth dall'interno
@@ -99,6 +137,7 @@ public class MainActivity extends AppCompatActivity {
                 if (!mBluetoothAdapter.isEnabled()) {
                     mBluetoothAdapter.enable();
                     req.setText("Bluetooth abled");
+
                 } else {
                     mBluetoothAdapter.disable();
                     req.setText("Bluetooth disabled");
@@ -106,12 +145,46 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==BLUETOOTH_ON && resultCode==RESULT_OK)
+        {
+            load();
+        }
+    }
+    public void scan(View v)
+    {
+        if (!btAdapter.isEnabled())
+        {
+            Intent turnOn = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(turnOn, BLUETOOTH_ON);
+        }
+        else
+            load();
+    }
+    private void load()
+    {
+        btAdapter.startDiscovery();
+      //  Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            // Loop through paired devices
+            //for (BluetoothDevice device : pairedDevices) {
+                // Add the name and address to an array adapter to show in a ListView
+
+        //    }
+        }
+
+
+
     //qui la distruzione
     @Override
     protected void onDestroy() {
         super.onDestroy();
         unregisterReceiver(mBroadcastReceiver1);
+
     }
+
     }
 
 
